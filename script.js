fbc4f5eef49f1e90fc33b0c520855c5b5ec04f29
Tory Fernandez <toryfernandez@Torys-Air.lan>
2023-07-22 13:55:18 -1000

    Manipulating DOM with JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a Random Quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+   authorText.textContent = quote.author;
+   quoteText.textContent = quote.text;
+
+// Check Quote length to determine styling
+if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+} else {
+    quoteText.classList.remove('long-quote');
+}
+
 }
 
 // Get Quotes From API
@@ -18,6 +33,16 @@ async function getQuotes() {
       // Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote () {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
